25639ab1375469b83bf38618cc7912fe87a45eed
lyubovrosato <lyubov.rosato@gmail.com>
2019-01-18 20:55:58 -0500

    committing new function to HW

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("Good Evening");
+        System.out.println("It is a holiday");
     }
 }
